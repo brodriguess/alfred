4266e08ca1e93b8900aa90d0b761c5ce45c03acd
Bruno Gastadi <gramado.f@gmail.com>
2017-06-16 09:06:18 -0300

    Cotação do Dolar e ajustes na piada

--- cavernabot.php
+++ cavernabot.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * DEFINE TOKEN E API
  */
@@ -39,51 +40,19 @@ function processaMensagem($message, $alfred) {
          */
         if (substr($intent[0], 0, 5) == 'piada') {
             /*
-             * PIADAS DINAMICAS
+             * PIADAS DINAMICAS 
+             * @bgastaldi
              */
-            $ch = curl_init();
-            $timeout = 0;
-            curl_setopt($ch, CURLOPT_URL, 'http://aspiadas.com/randomjoke.php');
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-            curl_setopt($ch, CURLOPT_ENCODING ,"utf8");
-            $output = curl_exec ($ch);
-            curl_close($ch);
-            $pattern = '/<p>(([^.]|.)*?)<\/p>/';
-            preg_match_all($pattern, str_replace("<br />","\n",utf8_encode($output)), $matches);
-            if(isset($matches[1][0])){
-               $mensagem = $matches[1][0];
-            }else{
-                $r = rand(0, 23);
-                $piadas = array(
-                    "Dois litros de leite atravessaram a rua e foram atropelados. Um morreu, o outro não, por quê?\nPor que um deles era Longa Vida.",
-                    "Por que o elefante não pega fogo?\nPorque ele já é cinza.",
-                    "Se o cachorro tivesse religião, qual seria?\nCão-domblé.",
-                    "O que o cavalo foi fazer no orelhão?\nPassar um trote.",
-                    "O que dá o cruzamento de pão, queijo e um macaco?\nX-panzé",
-                    "O que o tomate foi fazer no banco?\nFoi tirar extrato.",
-                    "O que a galinha foi fazer na igreja?\nAssistir a Missa do Galo.",
-                    "Como as enzimas se reproduzem?\nFica uma enzima da outra",
-                    "Por que a Coca-Cola e a Fanta se dão muito bem?\nPorque se a Fanta quebra, a Coca-Cola!",
-                    "Por que não é bom guardar o quibe no freezer?\nPorque lá dentro ele esfirra.",
-                    "Por que as plantas novinhas não falam?\nPorque elas são mudinhas.",
-                    "Por que o galo canta de olhos fechados?\nPorque ele já sabe a letra da música de cor",
-                    "Por que o Batman colocou o batmóvel no seguro?\nPorque ele tem medo que robin",
-                    "Por que Perón não teve filhos?\nPorque sua mulher Evita!",
-                    "Como o Batman faz para que abram a bat-caverna?\nEle bat-palma.",
-                    "Como se faz omelete de chocolate?\nCom ovos de páscoa.",
-                    "Por que na Argentina as vacas vivem olhando pro céu?\nPorque tem 'Boi nos Aires'",
-                    "Por que o Maradona não gosta que chamem ele de craque?\nPorque ele prefere cocaína...",
-                    "Para que servem óculos verdes?\nPara verde perto...",
-                    "Para que servem óculos vermelhos?\nPara vermelhor...",
-                    "Para que servem óculos marrons?\nPara ver marromenos...",
-                    "Por que a mulher do Hulk divorciou-se dele?\nPorque ela queria um homem mais maduro...",
-                    "Por que o jacaré tirou o jacarezinho da escola?\nPorque ele réptil de ano.",
-                    "Você sabe qual e o contrário de volátil?\nVem cá sobrinho.",
-                    "Como se fala topless em chinês?\nXem-chu-tian."
-                );
-                $mensagem = $piadas[$r];
-            }
+            $return = getPage('http://aspiadas.com/randomjoke.php');
+            preg_match_all('/<p>(([^.]|.)*?)<\/p>/', str_replace("<br />", "", utf8_encode($return)), $matches);
+            echo $mensagem = (isset($matches[1][0])) ? $matches[1][0] : "Desculpe patrão {$user}, hoje não estou conseguindo contar piadas...";
+        } else if (substr(strtolower($intent[0]), 0, 6) == 'dolar' || substr($intent[0], 0, 7) == 'usd' || substr(strtolower($intent[0]), 0, 6) == 'dólar') {
+            /*
+             * COTACAO DO DOLAR
+             * @bgastaldi
+             */
+            $dolar = json_decode(getPage('http://api.promasters.net.br/cotacao/v1/valores?moedas=USD&alt=json'), true);
+            $mensagem = isset($dolar['valores']['USD']['valor']) ? "Patrão {$user}, o valor do dolar agora é ".$dolar['valores']['USD']['valor']."." : "Desculpe patrão {$user}, ainda não li o jornal hoje!";
         } else if (substr(strtolower($intent[0]), 0, 6) == 'batman' || substr($intent[0], 0, 7) == 'bat-man') {
             $mensagem = "Não conheço nenhum Batman. Apenas trabalho aqui.";
         } else if (substr(strtolower($intent[0]), 0, 4) == 'time' || substr($intent[0], 0, 7) == 'futebol') {
@@ -124,16 +93,32 @@ function processaMensagem($message, $alfred) {
     $replymarkup = false;
     if ($mensagem != "") {
         if ($replymarkup) {
-            enviaResposta("sendMessage", array('parse_mode' => 'HTML','chat_id' => $destino,'disable_web_page_preview' => true,'text' => $mensagem,'reply_markup' => $replymarkup));
+            enviaResposta("sendMessage", array('parse_mode' => 'HTML', 'chat_id' => $destino, 'disable_web_page_preview' => true, 'text' => $mensagem, 'reply_markup' => $replymarkup));
         } else {
-            enviaResposta("sendMessage", array('parse_mode' => 'HTML','chat_id' => $destino,'disable_web_page_preview' => true,'text' => $mensagem));
+            enviaResposta("sendMessage", array('parse_mode' => 'HTML', 'chat_id' => $destino, 'disable_web_page_preview' => true, 'text' => $mensagem));
         }
     }
 }
 
 /*
+ * GET PAGE
+ */
+
+function getPage($url) {
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_ENCODING, "utf8");
+    curl_setopt($curl, CURLOPT_USERAGENT,'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:7.0.1) Gecko/20100101 Firefox/7.0.1');
+    $output = curl_exec($curl);
+    curl_close($curl);
+    return $output;
+}
+
+/*
  * FUNCAO ENVIAR RESPOSTA
  */
+
 function enviaResposta($metodo, $parametros) {
     $opcoes = array(
         'http' => array(
@@ -159,16 +144,17 @@ $update = json_decode($update_response, true);
 if (isset($update['callback_query'])) {
     processaCallbackQuery($update['callback_query']);
 }
-if (strpos(strtolower($update['message']['text']), strtolower('alfred')) !== false) {
-    $alfred = true;
-} else {
-    $alfred = false;
-}
+
+/*
+ * ATIVANDO O BOT
+ */
+$alfred = (strpos(strtolower($update['message']['text']), strtolower('alfred')) !== false) ? true : false;
+
 if (isset($update['message']['text']) && (substr($update['message']['text'], 0, 1) == '/' || $alfred/* substr(strtolower($update['message']['text']),0,6) == 'alfred' */)) {
     processaMensagem($update['message'], $alfred);
 }
 if (isset($update['message']['new_chat_member'])) {
-    enviaResposta("sendMessage", array('parse_mode' => 'HTML','chat_id' => $update['message']['chat']['id'],'disable_web_page_preview' => true,'text' => "Seja bem-vindo, patrão <b>{$update['message']['new_chat_member']['first_name']}</b> sinta-se a vontade. Aqui você pode falar de assuntos relacionados a: ChatBots, PLN, IA, Facebook Messenger, Slack, Telegram...\nCríticas e sugestões serão sempre bem vindas e faz a comunidade evoluir. Qualquer dúvida é só falar."));
+    enviaResposta("sendMessage", array('parse_mode' => 'HTML', 'chat_id' => $update['message']['chat']['id'], 'disable_web_page_preview' => true, 'text' => "Seja bem-vindo, patrão <b>{$update['message']['new_chat_member']['first_name']}</b> sinta-se a vontade. Aqui você pode falar de assuntos relacionados a: ChatBots, PLN, IA, Facebook Messenger, Slack, Telegram...\nCríticas e sugestões serão sempre bem vindas e faz a comunidade evoluir. Qualquer dúvida é só falar."));
 }
 /*
  * MENSAGEM PARA SITE
