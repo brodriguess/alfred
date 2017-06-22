bf0d41256b3c51064c907e7d583e61f7325d5226
felipe bastos <felipebastosweb@gmail.com>
2017-06-21 21:48:20 -0300

    Update habilidades.php

--- habilidades.php
+++ habilidades.php
@@ -35,7 +35,7 @@ function dolar()
 {
     $dolar = json_decode(file_get_contents('http://api.promasters.net.br/cotacao/v1/valores?moedas=USD&alt=json'));
     enviaResposta("sendMessage", array('parse_mode' => 'HTML', 'chat_id' => $args['destino'], 'disable_web_page_preview' => true,
-        'text' => "Patrão {$args['user']}, o dólar hoje estar custando:\n".
+        'text' => "Patrão {$args['user']}, o dólar hoje está custando:\n".
             'R$ '.$dolar->valores->USD->valor
     ));
 }
@@ -44,8 +44,9 @@ function euro()
 {
     $euro = json_decode(file_get_contents('http://api.promasters.net.br/cotacao/v1/valores?moedas=EUR&alt=json'));
     enviaResposta("sendMessage", array('parse_mode' => 'HTML', 'chat_id' => $args['destino'], 'disable_web_page_preview' => true,
-        'text' => "Patrão {$args['user']}, o euro hoje estar custando:\n".
-            'R$ '.$euro->valores->EUR->valor
+        'text' => "Patrão {$args['user']}, o euro hoje está custando:\n".
+            //'R$ '.$euro->valores->EUR->valor
+            file_get_contents('http://api.promasters.net.br/cotacao/v1/valores?moedas=EUR&alt=json')
     ));
 }
 
