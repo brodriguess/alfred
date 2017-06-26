4fa591261662e196110c5d68341a5a38d11dbf8f
felipe bastos <felipebastosweb@gmail.com>
2017-06-26 16:13:40 -0300

    Update and rename photo.php to photosize.php

--- telegram/message/photo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace BotCaverna\Telegram\Message;
-
-class Photo extends Message
-{
-    protected $photo; // Array of PhotoSize
-}
---
+++ telegram/message/photosize.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BotCaverna\Telegram\Message;
+
+class PhotoSize
+{
+    protected $file_id;
+    protected $width;
+    protected $height;
+    protected $file_size;
+}
