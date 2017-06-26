4a94dc52b6530c0c05490686d0bfbbf923062259
felipe bastos <felipebastosweb@gmail.com>
2017-06-26 16:11:04 -0300

    Create document.php

---
+++ telegram/message/document.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace BotCaverna\Telegram\Message;
+
+class Document
+{
+    protected $file_id;	// String
+    protected $thumb; // PhotoSize
+    protected $file_name; //String
+    protected $mime_type; //String
+    protected $file_size; //Integer
+}
