9fb6a691dadb8c8f5c3cacef5ea6ca74c1f4866a
felipe bastos <felipebastosweb@gmail.com>
2017-06-26 16:04:26 -0300

    Update and rename telegram/message.php to telegram/message/message.php

--- telegram/message.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace BotCaverna\Telegram;
-
-class Message
-{
-    //TODO: implemente https://core.telegram.org/bots/api#message
-}
---
+++ telegram/message/message.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BotCaverna\Telegram\Message;
+
+class Message
+{
+    //TODO: implemente https://core.telegram.org/bots/api#message
+    
+    protected $message_id; // int
+    protected $from; // User
+    protected $date; // int
+    protected $chat; // Chat
+    
+}
