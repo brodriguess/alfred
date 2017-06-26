09ac36238d1146c3e1d2de36f4752226d574478d
felipe bastos <felipebastosweb@gmail.com>
2017-06-26 16:21:42 -0300

    Update message.php

--- telegram/message/message.php
+++ telegram/message/message.php
@@ -11,4 +11,70 @@ class Message
     protected $date; // int
     protected $chat; // Chat
     
+    protected $text; // String
+    
+    // Documents, Photos e Videos
+    protected $document; // Document
+    protected $photo; // PhotoSize
+    protected $caption; // String
+    
+    public function setMessageID()
+    {
+        
+    }
+    
+    public function getMessageID()
+    {
+        
+    }
+    
+    public function setFrom()
+    {
+        
+    }
+    
+    public function getFrom()
+    {
+        
+    }
+    
+    public function setDate()
+    {
+        
+    }
+    
+    public function getDate()
+    {
+        
+    }
+    
+    public function setChat(Chat $chat)
+    {
+        $this->chat = $chat;
+    }
+    
+    public function getChat()
+    {
+        return $this->chat;
+    }
+    
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+    
+    public function getText()
+    {
+        return $this->text;
+    }
+    
+    public function setDocument(Document $document)
+    {
+        $this->document = $document;
+    }
+    public function getDocument()
+    {
+        return $this->document;
+    }
+    
 }
