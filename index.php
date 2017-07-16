c616a14d346cd71c8386881c2dc9e8bf1483403f
Bruno Gastadi <gramado.f@gmail.com>
2017-07-16 18:59:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
-<?php
+/*
+require 'habilidades.php';
+
+/*
+ * PEGANDO A MENSAGEM 
+ *
+if (isset($update["result"]["action"])) {
+    processMessage($update, $platform);
+}
 
 /*
  * PROCESSANDO A MENSAGEM 
- * QUE CHEGA DO BOT
- */
-function processMessage($update) {
-    if($update["result"]["action"] == "buscar"){
-        sendMessage(array(
-            "source" => $update["result"]["source"],
-            "speech" => "TESTE",
-            "displayText" => "TESTE",
-            "contextOut" => array()
-        ));
+ *
+function processMessage($update, $platform) {
+    if ($update["result"]["action"] == "moeda.consulta") {
+        $array = moedaConsulta($update);
+        sendMessage($array);
+    }else if ($update["result"]["action"] == "contar.piada") {
+        $array = contarPiada($update);
+        sendMessage($array);
     }
 }
+
 /*
  * FUNÇÃO PARA ENVIAR A MENSAGEM
- */
+ *
 function sendMessage($parameters) {
     echo json_encode($parameters);
 }
+
 /*
- * PEGANDO A REQUISIÇÃO
- */
+ * RECEBE UPDATES
+ *
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
-    processMessage($update);
-}
-
+*/
