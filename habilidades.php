153a4ca11f2ab7ad8d571849672a9b58281fb1b2
Bruno Gastadi <gramado.f@gmail.com>
2017-07-16 21:06:31 -0300

    Update habilidades.php

--- habilidades.php
+++ habilidades.php
@@ -1,18 +1,21 @@
+/*
+ *  FUNÇÕES
+ */
 function moeda($update = array())
 {
     $mensagem = array();
     if($update['result']['parameters']['moeda'] == 'EUR' or $update['result']['parameters']['moeda'] == 'USD'){
         $SLG = ($update['result']['parameters']['moeda'] == 'EUR') ? 'EURO' : 'DÓLAR';
-        $moeda = json_decode(file_get_contents('http://api.promasters.net.br/cotacao/v1/valores?moedas={$update['result']['parameters']['moeda']}&alt=json'), true);
+        $moeda = json_decode(file_get_contents('http://api.promasters.net.br/cotacao/v1/valores?moedas='.$update['result']['parameters']['moeda'].'&alt=json'), true);
         $mensagem[] = array(
             'type' => 0,
-            'speech' => 'Patrão, o {$SLG} hoje está custando R$ {$moeda['valores'][$update['result']['parameters']['moeda']]['valor']}'
+            'speech' => 'Patrão, o {$SLG} hoje está custando R$ '.$moeda['valores'][$update['result']['parameters']['moeda']]['valor'].'.'
         );
     }else{
         $bitcoin = json_decode(file_get_contents('https://blockchain.info/pt/ticker'), true);
         $mensagem[] = array(
             'type' => 0,
-            'speech' => 'Patrão, esta é a cotação do bitcoin neste instante\n {$bitcoin['USD']['symbol']} {$bitcoin['USD']['sell']}, {$bitcoin['EUR']['symbol']} {$bitcoin['EUR']['sell']}, {$bitcoin['BRL']['symbol']} {$bitcoin['BRL']['sell']}.'
+            'speech' => 'Patrão, esta é a cotação do bitcoin neste instante\n '.$bitcoin['USD']['symbol'].' '.$bitcoin['USD']['sell'].', '.$bitcoin['EUR']['symbol'].' '.$bitcoin['EUR']['sell'].', '.$bitcoin['BRL']['symbol'].' '.$bitcoin['BRL']['sell'].'.'
         );    
     }
     
