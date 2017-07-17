fbf3ffb80af920d185b38626584d192e9c22b429
Bruno Gastadi <gramado.f@gmail.com>
2017-07-16 21:40:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,28 @@
 /*
- * HABILIDADES
- */
-//require 'habilidades.php';
-
-/*
- * RECEBE UPDATES
- */
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-
-/*
- * PEGANDO A MENSAGEM 
- */
-if (isset($update["result"]["action"])) {
-    processMessage($update);
-}
-
-/*
  * PROCESSANDO A MENSAGEM 
+ * QUE CHEGA DO BOT
  */
 function processMessage($update) {
-    if ($update["result"]["action"] == "moeda") {
-        //$array = moeda($update);
-        $array =  array(
+    if($update["result"]["action"] == "moeda){
+        sendMessage(array(
             "source" => $update["result"]["source"],
             "speech" => "..........TEXT HERE...........",
             "displayText" => ".........TEXT HERE...........",
             "contextOut" => array()
-        );
-        sendMessage($array);
-    }else if ($update["result"]["action"] == "piada") {
-        //$array = piada($update);
-        //sendMessage($array);
-    }else if ($update["result"]["action"] == "tempo") {
-        //$array = tempo($update);
-        //sendMessage($array);
+        ));
     }
 }
-
 /*
  * FUNÇÃO PARA ENVIAR A MENSAGEM
  */
 function sendMessage($parameters) {
     echo json_encode($parameters);
+}
+/*
+ * PEGANDO A REQUISIÇÃO
+ */
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
 }
